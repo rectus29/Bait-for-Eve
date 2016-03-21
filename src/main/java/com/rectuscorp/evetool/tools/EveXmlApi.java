7da8fa1796338fc70b5f521c963d28c04fa5aabc
a.bernard <a.bernard@e-deal.com>
2016-03-21 15:04:06 +0100

    instancaite service from scratch

--- src/main/java/com/rectuscorp/evetool/tools/EveXmlApi.java
+++ src/main/java/com/rectuscorp/evetool/tools/EveXmlApi.java
@@ -4,6 +4,7 @@ import com.rectuscorp.evetool.entities.core.ApiKey;
 import com.rectuscorp.evetool.entities.core.Character;
 import com.rectuscorp.evetool.entities.crest.Corporation;
 import com.rectuscorp.evetool.service.IserviceCorporation;
+import com.rectuscorp.evetool.spring.AppContext;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.logging.log4j.LogManager;
@@ -14,6 +15,8 @@ import org.dom4j.dom.DOMDocument;
 import org.dom4j.dom.DOMDocumentFactory;
 import org.dom4j.dom.DOMElement;
 import org.dom4j.io.SAXReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -23,10 +26,14 @@ public class EveXmlApi {
 
 	private static final Logger log = LogManager.getLogger(EveXmlApi.class);
 	public static String API_URL = "https://api.eveonline.com/";
-	@SpringBean(name = "serviceCorporation")
-	private static IserviceCorporation serviceCorporation;
 	private static EveXmlApi ourInstance;
 	private HttpClient client = new HttpClient();
+	private static IserviceCorporation serviceCorporation;
+
+	public EveXmlApi() {
+		ApplicationContext context = AppContext.getApplicationContext();
+		serviceCorporation = (IserviceCorporation) context.getBean("serviceCorporation");
+	}
 
 	/**
 	 * Singleton Getter
@@ -57,7 +64,7 @@ public class EveXmlApi {
 				Character character = new Character();
 				character.setName(charElement.getAttribute("name"));
 				if (charElement.getAttribute("corporationID") != null) {
-					Application.get()
+					//Application.get()
 					Corporation corpo = serviceCorporation.get(Long.parseLong(charElement.getAttribute("corporationID")));
 					character.setCorporation(corpo);
 				}
